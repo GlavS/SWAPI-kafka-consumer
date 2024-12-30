46a3da30d92b610c6e4d414b0380e946a7c97db7
GlavS <pianoforte2@yandex.ru>
2024-12-30 12:29:26 +0300

    Initial

---
+++ build.gradle.kts
@@ -0,0 +1,59 @@
+plugins {
+    id("java")
+    id("org.springframework.boot") version "3.4.0"
+    id("io.spring.dependency-management") version "1.1.6"
+    id ("com.google.cloud.tools.jib") version "3.4.4"
+}
+
+group = "ru.glavs"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+jib {
+    from{
+        image = "bellsoft/liberica-openjdk-alpine-musl:21.0.1"
+    }
+    to {
+        image = "glavs/swapi-kafka-consumer"
+    }
+}
+
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-websocket")
+    compileOnly("org.projectlombok:lombok")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    annotationProcessor("org.projectlombok:lombok")
+
+    implementation("org.webjars:webjars-locator-core")
+    implementation("org.webjars:sockjs-client:1.5.1")
+    implementation("org.webjars:stomp-websocket:2.3.4")
+    implementation("org.webjars:bootstrap:5.2.3")
+
+    implementation("org.apache.kafka:kafka-clients:3.9.0")
+//    implementation("ch.qos.logback:logback-classic")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.18.2")
+
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
